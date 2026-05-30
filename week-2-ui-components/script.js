f021158ab6e1568dab76066dcdde99ee4cb72191
Shubham <vysshubham04@gmail.com>
2026-05-30 23:09:16 +0530

    Improved accordion behavior with single active panel

--- week-2-ui-components/script.js
+++ week-2-ui-components/script.js
@@ -1,1 +1,26 @@
-console.log("Accordion Project Started");
+const buttons = document.querySelectorAll(".accordion-btn");
+
+buttons.forEach((button) => {
+
+    button.addEventListener("click", () => {
+
+        const content = button.nextElementSibling;
+
+        document.querySelectorAll(".accordion-content").forEach((item) => {
+
+            if(item !== content){
+                item.style.display = "none";
+            }
+
+        });
+
+        if(content.style.display === "block"){
+            content.style.display = "none";
+        }
+        else{
+            content.style.display = "block";
+        }
+
+    });
+
+});
