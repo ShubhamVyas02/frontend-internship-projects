a07fd5cd889bf52b710a5ed9a30a11ccf61ca8d4
Shubham <vysshubham04@gmail.com>
2026-05-31 18:34:44 +0530

    Added accordion accessibility improvements

--- week-2-ui-components/script.js
+++ week-2-ui-components/script.js
@@ -6,19 +6,41 @@ buttons.forEach((button) => {
 
         const content = button.nextElementSibling;
 
+        // Close all other accordion sections
         document.querySelectorAll(".accordion-content").forEach((item) => {
 
-            if(item !== content){
+            if (item !== content) {
+
                 item.style.display = "none";
+
+                item.previousElementSibling.setAttribute(
+                    "aria-expanded",
+                    "false"
+                );
+
             }
 
         });
 
-        if(content.style.display === "block"){
+        // Toggle current accordion section
+        if (content.style.display === "block") {
+
             content.style.display = "none";
-        }
-        else{
+
+            button.setAttribute(
+                "aria-expanded",
+                "false"
+            );
+
+        } else {
+
             content.style.display = "block";
+
+            button.setAttribute(
+                "aria-expanded",
+                "true"
+            );
+
         }
 
     });
